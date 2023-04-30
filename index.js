ccc4932a550d19c1c543c720e5b77b1f2a13f3ec
Jason <jason@Jasons-MacBook-Air.local>
2023-04-30 14:53:11 -0700

    added a function newImage and added 4 more images

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function newImage(){
+function newImage(img, left, bottom){
 
-    let greenCharacter = document.createElement('img')
-    greenCharacter.src = 'assets/green-character.gif'
-    greenCharacter.style.position = 'fixed'
-    greenCharacter.style.left = '100px'
-    greenCharacter.style.bottom = '100px'
-    document.body.append(greenCharacter)
+    let image = document.createElement('img')
+    image.src = img
+    image.style.position = 'fixed'
+    image.style.left = left +'px'
+    image.style.bottom = bottom +'px'
+    document.body.append(image)
+    return image
     
-    let pineTree = document.createElement('img')
-    pineTree.src = 'assets/pine-tree.png'
-    pineTree.style.position = 'fixed'
-    pineTree.style.left = '450px'
-    pineTree.style.bottom = '200px'
-    document.body.append(pineTree) 
 }
-newImage()
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
