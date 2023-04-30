8505c05d53440cc62d945480b233099e164d1071
Jason <jason@Jasons-MacBook-Air.local>
2023-04-30 15:14:16 -0700

    minor changes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function newImage(img, left, bottom){
     let image = document.createElement('img')
     image.src = img
     image.style.position = 'fixed'
-    image.style.left = left +'px'
-    image.style.bottom = bottom +'px'
+    image.style.left = left
+    image.style.bottom = bottom
     document.body.append(image)
     return image
 
@@ -18,13 +18,13 @@ image.remove()
 }) 
 }
 
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
 
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
