c5406accb29a8fda97066b85ef333815c083a964
Jason <jason@Jasons-MacBook-Air.local>
2023-04-30 15:04:22 -0700

    created a newItem function

--- index.js
+++ index.js
@@ -10,17 +10,13 @@ function newImage(img, left, bottom){
 
 }
 
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
+function newItem(img, left, bottom){
+let image = newImage(img, left, bottom)
 
-sword.addEventListener('click', function(){
-sword.remove()
+image.addEventListener('click', function(){
+image.remove()
 }) 
-
+}
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
@@ -28,3 +24,5 @@ newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
