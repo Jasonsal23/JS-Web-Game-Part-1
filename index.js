d1616c96af62b5e030f19c74729aae6ea792d8a0
Jason <jason@Jasons-MacBook-Air.local>
2023-05-02 19:23:15 -0700

    created a scope to place items in an inventory

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let inventory=document.createElement('div')
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -12,11 +14,13 @@ function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
     item.addEventListener('click',function(){
         item.remove()
+        let inventoryItem=document.createElement('img')
+        inventoryItem.src =url
+        inventory.append(inventoryItem)
     })
 }
 
 function newInventory(){
-    let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
